62ce667bdac1839fa2bf7041b94d833005644ddb
Alena <livionak@gmail.com>
2025-09-13 17:53:32 +0200

    [fix] mDB: change correct folder for init script, other configs

---
+++ inception/srcs/requirements/mariadb/tools/init_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# setup database, users, permissions, etc.. 
+echo "I am running"
+
+# create database for ENV_XXX
+# create user if not exist with passw 
+# grant permssions to database for user
+# if init.sql exists - import db  
+
+
+# run everything in CMD 
+exec "$@"
